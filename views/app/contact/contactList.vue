bb137141dd97e6c5cf3a8f07f2302f78f28d3578
Rohini Kawadkar <140095255+Rohin012@users.noreply.github.com>
2024-01-29 16:58:54 +0530

    Update contactList.vue

--- views/app/contact/contactList.vue
+++ views/app/contact/contactList.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <TableSkeltion :count=perpage v-if="isSkeletion" />
+    <TableSkeltion :count=perpage v-if="isSkeletion" class=""/>
     <SimpleBar>
-      <Card noborder>
-        <vue-good-table class="-mx-6 " :columns="columns" styleClass=" vgt-table v-middle lesspadding2 listview"
+      <Card className="py-4 h-[75vh] w-full" >
+        <vue-good-table class="-mx-6 min " :columns="columns" styleClass=" vgt-table v-middle lesspadding2 listview"
           :rows="contacts" max-height="72vh" :fixed-header="true" :pagination-options="{
             enabled: true,
             perPage: perpage,
@@ -11,37 +11,51 @@
   enabled: false,
 
 }" v-if="!isSkeletion">
-          <template v-slot:table-row="props">
-            <span v-if="props.column.field == 'email'" class="text-slate-500 lowercase dark:text-slate-300">
+          <template v-slot:table-row="props" class="w-3/4  mx-auto" >
+            <span v-if="props.column.field == 'email'" class="text-[12px] text-slate-500 lowercase dark:text-slate-300">
               {{ props.row.email }}
             </span>
             <span v-if="props.column.field == 'tags'" class="block w-full">
               <div class="flex">
-                <template v-for="(tag, tagIndex) in props.row.tags" :key="tagIndex">
+                <template v-for="(tags, tagIndex) in props.row.tags" :key="tagIndex">
                   <div class="px-2 py-1 mr-1 bg-gray-200 text-gray-800 rounded">
-                    {{ tag }}
+                    {{ props.row.tags.value }}
                   </div>
                   <span v-if="tagIndex !== props.row.tags.length - 1"></span>
                 </template>
               </div>
             </span>
-            <span v-if="props.column.field == 'createDate'">
-              {{ props.row.createDate }}
+            <span v-if="props.column.field == 'lastmessageDate'">
+              
             </span>
-            <span v-if="props.column.field == 'lastActivity'" class="text-slate-500 dark:text-slate-300">
-              {{ props.row.lastActivity }}
+            <span v-if="props.column.field == 'lastmessageTime'" class="text-slate-500 dark:text-slate-300">
+              
+              {{ props.row.lastmessageDate }} {{ props.row.lastmessageTime }}
             </span>
             <span v-if="props.column.field == 'action'">
-              <div class="space-x-3">
-                <Tooltip placement="top" arrow :theme="action.theme" btnClass="my-3" v-for="(action, i) in actions">
-                  <template #button>
-                    <div class="action-btn">
-                      <Icon :icon="action.icon" />
-                    </div>
-                  </template>
-                  <span> {{ action.name }}</span>
-                </Tooltip>
-              </div>
+              <Dropdown classMenuItems=" w-[140px]">
+              <span class="text-xl"
+                ><Icon icon="heroicons-outline:dots-vertical"
+              /></span>
+              <template v-slot:menus>
+                <MenuItem v-for="(item, i) in actions" :key="i">
+                  <div 
+                    :class="`
+                
+                  ${
+                    item.name === 'delete'
+                      ? 'bg-danger-500 text-danger-500 bg-opacity-30   hover:bg-opacity-100 hover:text-white'
+                      : 'hover:bg-slate-900 hover:text-white dark:hover:bg-slate-600 dark:hover:bg-opacity-50'
+                  }
+                   w-full border-b border-b-gray-500 border-opacity-10 px-4 py-2 text-sm  last:mb-0 cursor-pointer first:rounded-t last:rounded-b flex  space-x-2 items-center rtl:space-x-reverse `
+                  
+                  " @click.prevent="handleActionClick(item, props.row)">
+                    <span class="text-base"><Icon :icon="item.icon" /></span>
+                    <span>{{ item.name }}</span>
+                  </div>
+                </MenuItem>
+              </template>
+            </Dropdown>
             </span>
           </template>
           <template #pagination-bottom="props">
@@ -55,6 +69,12 @@
       </Card>
     </SimpleBar>
   </div>
+             <Modal
+             :activeModal="showModal"
+             @close="closeUpdate()"
+              title="Edit Profile">
+              <contactProfile :contacts="contacts" />
+               </Modal>
 </template>
 <script>
 import Card from "@/components/Card";
@@ -67,6 +87,8 @@ import TableSkeltion from "@/components/Skeleton/Table";
 import { SimpleBar } from "simplebar-vue3";
 import { ref, onMounted } from 'vue';
 import { useContactStore } from "@/store/contact";
+import contactProfile from "./contactProfile.vue"
+import Modal from "@/components/Modal";
 
 export default {
   components: {
@@ -78,6 +100,8 @@ export default {
     Tooltip,
     TableSkeltion,
     SimpleBar,
+    contactProfile,
+    Modal 
   },
 
   data() {
@@ -85,6 +109,9 @@ export default {
       current: 1,
       perpage: 10,
       pageRange: 10,
+      // id:"",
+      // showModal:"",
+      
       actions: [
         {
           name: "View",
@@ -98,9 +125,7 @@ export default {
           name: "Edit",
           icon: "heroicons:pencil-square",
           theme: "dark",
-          doit: (data) => {
-            this.updateProject(data);
-          },
+          
         },
         {
           name: "Delete",
@@ -141,16 +166,17 @@ export default {
         },
         {
           label: "Create On",
-          field: "createDate",
+          field: "lastmessageDate",
         },
         {
           label: "Last Activity",
-          field: "lastActivity",
+          field: "lastmessageTime",
         },
         {
           label: "Action",
           field: "action",
         },
+       
       ],
       contacts: [],
     };
@@ -158,23 +184,63 @@ export default {
   async created() {
 
     const contactStore = useContactStore();
-    await contactStore.getContacts;
+    await contactStore.fetchContact();
 
     this.contacts = contactStore.contacts;
     this.updateSequence();
 
+
   },
   setup() {
     const isSkeletion = ref(true);
+    let handleActionClick = ref(true);
+    const showModal = ref(false);
+    const contactStore = useContactStore();
+    const contacts = ref([]);
+    const id = ref("")
 
-    onMounted(() => {
+     handleActionClick = (item, contact) => {
+      console.log('Contact:', contact);
+
+      if (contact && item.name === 'Edit') {
+        showModal.value = true;
+        id.value = contact.id; // Pass the contact id to the modal
+      } else if (item.name === 'delete') {
+        // Handle delete action
+      } else {
+        // Handle other actions
+      }
+    }
+
+    
+    
+
+    onMounted(async() => {
       setTimeout(() => {
         isSkeletion.value = false;
       }, 1000);
-    });
 
+      if (contactStore.contacts.length === 0) {
+        await contactStore.fetchContactId();
+      }
+      
+      contacts.value = contactStore.contacts;
+ })
+
+
+ const closeUpdate = () => {
+  console.log('Closing modal');
+      showModal.value= false;
+      id.value= ""; // Reset id when closing modal
+    };
     return {
-      isSkeletion, // Return isSkeletion in the setup function
+      isSkeletion,
+      handleActionClick,
+      showModal,
+      closeUpdate,
+      contacts,
+      id
+     // Return isSkeletion in the setup function
     };
   },
   methods: {  
@@ -184,9 +250,15 @@ export default {
           contact.no = index + 1;
         });
       }
-    }
+    },
+
+    
+    
+  },
+ 
   }
-};
+  
+
 </script>
 <style lang="scss">
 .listview {
